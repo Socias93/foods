fd4faf28f89af74163efc324a55d1c6ca5dbc439
Christian Socias <Socias_93@hotmail.com>
2025-11-11 14:30:07 +0100

    created pagination with functionality but not connected to tablelist

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,25 +1,39 @@
+import { useState } from "react";
+
 function Pagination() {
+  const [selectedPage, setSelectedPage] = useState(1);
+  const pageSize = 4;
+  const totalCount = 9;
+
+  const pageCount = Math.ceil(totalCount / pageSize);
+
+  let pages = range(1, pageCount);
+
   return (
     <nav aria-label="...">
       <ul className="pagination pagination-sm">
-        <li className="page-item active">
-          <a className="page-link" aria-current="page">
-            1
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#">
-            2
-          </a>
-        </li>
-        <li className="page-item">
-          <a className="page-link" href="#">
-            3
-          </a>
-        </li>
+        {pages.map((page) => (
+          <li className="page-item m-1">
+            <a
+              onClick={() => setSelectedPage(page)}
+              className={`bg-dark text-white clickable page-link rounded-2 ${
+                page === selectedPage ? "active bg-black" : "bg-grey"
+              }`}
+              aria-current="page">
+              {page}
+            </a>
+          </li>
+        ))}
       </ul>
     </nav>
   );
+}
+
+export function range(startNumber: number, endNumber: number): number[] {
+  let pages: number[] = [];
+
+  for (let count = startNumber; count <= endNumber; count++) pages.push(count);
+  return pages;
 }
 
 export default Pagination;
