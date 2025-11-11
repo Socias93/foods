1de19b226b0e36869f83f23402df8737ccd606a1
Christian Socias <Socias_93@hotmail.com>
2025-11-11 14:52:17 +0100

    created functionality to listgroup component, not connected to tablelist yet

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,12 +1,21 @@
+import { useState } from "react";
 import { getCategories } from "../services/fakeCategoryService";
 
+const DEFAULT_CATEGORY = { _id: "", name: "All Categories" };
+
 function ListGroup() {
-  const categories = getCategories();
+  const [selectedCategory, setSelectedCategory] = useState(DEFAULT_CATEGORY);
+  const categories = [DEFAULT_CATEGORY, ...getCategories()];
 
   return (
     <ul className="list-group list-group-flush">
       {categories.map((category) => (
-        <li key={category._id} className="clickable list-group-item">
+        <li
+          onClick={() => setSelectedCategory(category)}
+          key={category._id}
+          className={`clickable list-group-item rounded-3 ${
+            selectedCategory._id === category._id ? "active bg-black" : ""
+          }`}>
           {category.name}
         </li>
       ))}
