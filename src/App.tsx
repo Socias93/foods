a41f967e2f9b470914b61d52212b3c22740e323e
Christian Socias <Socias_93@hotmail.com>
2025-11-11 13:50:53 +0100

    added fake service for categories and food and created a tablelist

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,32 @@
 import "./App.css";
+import { getFoods } from "./services/fakeFoodService";
 
 function App() {
-  return <></>;
+  const foods = getFoods();
+  return (
+    <>
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">Name</th>
+            <th scope="col">Category</th>
+            <th scope="col">Price</th>
+            <th scope="col">Stock</th>
+          </tr>
+        </thead>
+        <tbody className="table-group-divider">
+          {foods.map((food) => (
+            <tr key={food._id}>
+              <td> {food.name} </td>
+              <td> {food.category.name} </td>
+              <td> {food.price} </td>
+              <td> {food.numberInStock} </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
 
 export default App;
