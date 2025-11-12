43282c680bf148cad294ad652dd9b7c6b94bacd7
Christian Socias <Socias_93@hotmail.com>
2025-11-12 16:10:02 +0100

    Fix: show sort icon on active column

--- src/components/TableHeader.tsx
+++ src/components/TableHeader.tsx
@@ -1,5 +1,5 @@
 import { SortColumn } from "../pages/FoodsPage";
-import { Columns } from "./utils";
+import { Columns, TextColumn } from "./utils";
 
 interface Props {
   columns: Columns[];
@@ -9,7 +9,7 @@ interface Props {
 
 function TableHeader({ columns, sortColumn, setSortColumn }: Props) {
   function handleSort(path: string) {
-    if (sortColumn.path === path) {
+    if (sortColumn.path == path) {
       sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
     } else {
       sortColumn.path = path;
@@ -18,16 +18,27 @@ function TableHeader({ columns, sortColumn, setSortColumn }: Props) {
     setSortColumn({ ...sortColumn });
   }
 
+  const sortUp = <i className="fa-solid fa-sort-up"></i>;
+  const sortDown = <i className="fa-solid fa-sort-down"></i>;
+
+  function renderSortIcon(column: TextColumn) {
+    if (column.path !== sortColumn.path) return null;
+
+    return sortColumn.order === "asc" ? sortUp : sortDown;
+  }
+
   return (
     <thead>
       <tr>
         {columns.map((column) =>
           "path" in column ? (
             <th
-              onClick={() => handleSort(sortColumn.path)}
+              className="clickable"
+              onClick={() => handleSort(column.path)}
               key={column.path}
               scope="col">
               {column.label}
+              {renderSortIcon(column)}
             </th>
           ) : (
             <th key={column.key}> </th>
